f373bfeb0305956f167609aadbb01efdb5f22a31
sowmiyanarayani <sowmiya2692001@gmail.com>
2024-04-09 16:52:55 +0530

    firstCommit2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import { React } from 'react';
 import './App.scss';
 
+// eslint-disable-next-line no-magic-numbers
+const numbers = [1, 2, 3, 4, 5];
 const App = () =>
 	<div className="App">
-		Ready to start.
+		{ numbers.map((number, index) =>
+			<div key={ index }> { number } </div>) }
 	</div>;
 
 export default App;
